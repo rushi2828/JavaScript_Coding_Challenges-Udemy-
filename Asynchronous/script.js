4bce1e23c3da5f704e8f2e1c76e0a6d1312eca83
unknown <rushi2828@gmail.com>
2023-11-17 00:45:02 +0530

    Promises learning

--- Asynchronous/script.js
+++ Asynchronous/script.js
@@ -5,37 +5,120 @@
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
+/////////////////////////////////
 
-const getCountryData = country__name => {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country__name}`);
-  request.send();
+//   const html = `
+//   <article class="country">
+//   <img class="country__img" src="${data.flags.png}" />
+//   <div class="country__data">
+//     <h3 class="country__name">"${data.name.official}"</h3>
+//     <h4 class="country__region">${data.region}</h4>
+//     <p class="country__row"><span>👫</span>${(
+//       +data.population / 1000000
+//     ).toFixed(1)} people</p>
+//     <p class="country__row"><span>🗣️</span>${data.languages}</p>
+//     <p class="country__row"><span>💰</span>${data.currencies[0]}</p>
+//   </div>
+// </article> `;
 
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+// cretaed a funtion
+const renderCountry = function (data, className = '') {
+  const html = `
+        <article class="country ${className}">
+        <img class="country__img" src="${data.flags.png}" />
+        <div class="country__data">
+          <h3 class="country__name">"${data.name.official}"</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+          ).toFixed(1)} people</p>
+          <p class="country__row"><span>🗣️</span>${data.languages}</p>
+          <p class="country__row"><span>💰</span>${data.currencies[0]}</p>
+        </div>
+      </article> `;
 
-    const html = `
-  <article class="country">
-    <img class="country__img" src="${data.flags.png}"/>
-    <div class="country__data">
-      <h3 class="country__name">"${data.name.common}"</h3>
-      <h4 class="country__region">"${data.region}"</h4>
-      <p class="country__row">
-        <span>👫</span>"${(data.population / 1000000).toFixed(1)}"
-      </p>
-      <p class="country__row">
-        <span>🗣️</span>"${data.languages.hin}"
-      </p>
-      <p class="country__row">
-        <span>💰</span>"${data.currencies.INR.name}"
-      </p>
-    </div>
-  </article> `;
-
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
-  });
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
 };
 
-getCountryData('india');
+///////////////////////////////////////
+// Welcome to Callback Hell
+
+// const getCountryAndNeighbour = country => {
+//   // AJAX call country #1
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+
+//     //Rebder country
+//     renderCountry(data);
+
+//     // Get neighbour country 2
+//     const [neighbour] = data.borders;
+
+//     if (!neighbour) return;
+
+//     // AJAX call country 2
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+//     request2.send();
+
+//     request2.addEventListener('load', function () {
+//       const [data2] = JSON.parse(this.responseText);
+//       console.log(data2);
+
+//       renderCountry(data2, 'neighbour');
+//     });
+//   });
+// };
+
+// getCountryAndNeighbour('portugal');
+// // getCountryAndNeighbour('usa');
+// console.log('==============');
+
+///////////////////////////////////////
+// Promise -An object that is used to placeholder for the future resultof an async operation.
+// A Promise is in one of these states:
+
+// pending: initial state, neither fulfilled nor rejected.
+// fulfilled: meaning that the operation was completed successfully.
+// rejected: meaning that the operation failed.
+
+// const request = fetch('https://restcountries.com/v3.1/name/portugal');
+// console.log(request); //Promise {<pending>}
+
+// const getCountryData = function (country) = {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(function (response) = {
+//       console.log(response);
+//       // here .json() method is also returning new promise so that's why we are using .then(callback fn(){})
+//       return response.json();
+//     })
+//     .then(data => {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+// simplified version for above i.e with arrow function
+const getCountryData = country => {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // country # 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data[0], 'neighbour'));
+};
+
+getCountryData('portugal');
