7bc0fd1ef470094ebc472a1791baeb30d78278f6
unknown <rushi2828@gmail.com>
2023-11-16 20:46:58 +0530

    Added lines in async folder

--- Asynchronous/script.js
+++ Asynchronous/script.js
@@ -6,29 +6,36 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v3.1/name/india');
-request.send();
+const getCountryData = country__name => {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country__name}`);
+  request.send();
 
-request.addEventListener('load', function () {
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
 
-  const html = `
+    const html = `
   <article class="country">
-    <img class="country__img" src="${data.flag}"/>
+    <img class="country__img" src="${data.flags.png}"/>
     <div class="country__data">
-      <h3 class="country__name">INDIA</h3>
+      <h3 class="country__name">"${data.name.common}"</h3>
       <h4 class="country__region">"${data.region}"</h4>
       <p class="country__row">
-        <span>👫</span>POP people
+        <span>👫</span>"${(data.population / 1000000).toFixed(1)}"
       </p>
       <p class="country__row">
-        <span>🗣️</span>LANG
+        <span>🗣️</span>"${data.languages.hin}"
       </p>
       <p class="country__row">
-        <span>💰</span>CUR
+        <span>💰</span>"${data.currencies.INR.name}"
       </p>
     </div>
   </article> `;
-});
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('india');
